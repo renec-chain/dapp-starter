650077d84fd6cb1abdd3fc88c8933c4aa230197b
kientt6034 <kientrinh@remitano.com>
2023-06-05 00:12:18 +0700

    generic wallet path in deploy scripts

--- dev-scripts/deploy.sh
+++ dev-scripts/deploy.sh
@@ -8,4 +8,4 @@ else
 fi
 
 PROGRAM_NAME_UNDERSCORE=${PROGRAM_NAME//-/_}
-solana program deploy target/deploy/$PROGRAM_NAME_UNDERSCORE.so --keypair .wallets/$WALLET_NAME.json --url $CLUSTER_URL
+solana program deploy target/deploy/$PROGRAM_NAME_UNDERSCORE.so --keypair $WALLET_PATH/$WALLET_NAME.json --url $CLUSTER_URL
