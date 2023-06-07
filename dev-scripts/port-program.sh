2e452fdf15d1af663ef96977c864018ede6291fe
docongminh <minhdc@remitano.com>
2023-06-07 15:03:20 +0700

    ADD: program-id keypair

--- dev-scripts/port-program.sh
+++ dev-scripts/port-program.sh
@@ -2,8 +2,9 @@
 
 PROGRAM_FILE=$1
 KEYPAIR_PATH=$2
+PROGRAM_ID=$3
 
 echo "deploy program file path: $PROGRAM_FILE"
 echo "keypair path: $KEYPAIR_PATH"
 
-solana program deploy $PROGRAM_FILE --keypair $KEYPAIR_PATH --url $CLUSTER_URL
+solana program deploy $PROGRAM_FILE --keypair $KEYPAIR_PATH --program-id $PROGRAM_ID --url $CLUSTER_URL
