5b0ab468e874c6443d17d1fc6da86b9cebd638e7
docongminh <minhdc@remitano.com>
2023-06-07 15:07:42 +0700

    console log

--- dev-scripts/port-program.sh
+++ dev-scripts/port-program.sh
@@ -6,5 +6,6 @@ PROGRAM_ID=$3
 
 echo "deploy program file path: $PROGRAM_FILE"
 echo "keypair path: $KEYPAIR_PATH"
+echo "program id keypair path: $PROGRAM_ID"
 
 solana program deploy $PROGRAM_FILE --keypair $KEYPAIR_PATH --program-id $PROGRAM_ID --url $CLUSTER_URL
